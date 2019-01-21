f9d8aba98f0eb425629aba332b8d68b1bab07763
Yacinth <fevriercjjc@gmail.com>
2019-01-21 19:16:48 +0100

    update user.rb, test spec ok

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,26 @@
-class User
+require 'pry'
 
+class User
+  attr_accessor :email, :name
+  @@user_all = [] #on initialise la ariable de classe qui sera un compteur du nombre d'instance
+
+#   meilleure def initialize que celle de spec
+#   def initialize(email, name)
+#       @name = name
+#       @email = email
+#       @@user_all << @name      
+#   end
+
+def initialize(email_to_save)
+  @email = email_to_save
 end
+
+  def self.all
+    return @@user_all
+  end
+
+end #fin de ma classe
+
+#  binding.pry
+#  puts "end of file"
+
